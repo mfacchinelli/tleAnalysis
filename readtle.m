210d5b016f23d694ce935ae940b687d9194c4b0b
mfacchinelli <michele.facchinelli@yahoo.it>
2017-03-10 11:35:26 +0100

    Applied small changes

--- readtle.m
+++ readtle.m
@@ -20,16 +20,16 @@ mu = 398600.441e9;          % [m3/s2]   Earth gravitational parameter
 Te = 23*3600+56*60+4.1004;  % [s]       Earth sidereal day
 
 %...Decode lines
-satnum = read(3,1:2:end);
-i = str2double(read(3,2:2:end));
-O = str2double(read(4,2:2:end));
-e = str2double(read(5,2:2:end))./1e7;
-o = str2double(read(6,2:2:end));
-M = str2double(read(7,2:2:end));
-n = str2double(read(8,2:2:end));
-T = Te./n;
-a = ((T./(2*pi)).^2*mu).^(1/3);
-b = a.*sqrt(1-e.^2);
+satID = read(3,1:2:end);                % [-]       satellite identifier
+i = str2double(read(3,2:2:end));        % [deg]     inclination
+O = str2double(read(4,2:2:end));        % [deg]     right ascension of ascending node
+e = str2double(read(5,2:2:end))./1e7;   % [-]       eccentricity
+o = str2double(read(6,2:2:end));        % [deg]     argument of perigee
+M = str2double(read(7,2:2:end));        % [deg]     mean anomaly
+n = str2double(read(8,2:2:end));        % [rad/s]   mean motion
+T = Te./n;                              % [s]       period
+a = ((T./(2*pi)).^2*mu).^(1/3);         % [m]       semi-major axis
+b = a.*sqrt(1-e.^2);                    % [m]       semi-minor axis
 
 %...Combine Keplerian elements
 kepler = vertcat(a./1000,e,i,O,o,M);
