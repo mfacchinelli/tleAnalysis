bffc78644869e0e900ac00df49bdf87d41b6464b
mfacchinelli <michele.facchinelli@yahoo.it>
2017-03-10 19:02:47 +0100

    Applied small changes

--- main.m
+++ main.m
@@ -2,13 +2,14 @@ clear all; close all; clc;
 
 %% Input file 
 
-file = 'noaa-06.txt';       % NOAA 06   (full) doesn't work
-file = 'noaa.txt';          % NOAA 06   (reduced) works
-file = 'zarya.txt';         % ISS       (reduced) works
+% file = 'noaa-06';   % NOAA 06   (full) doesn't work
+file = 'noaa';      % NOAA 06   (reduced) works
+% file = 'zarya';     % ISS       (full) doesn't work
+% file = 'delfic3';   % Delfi C3  (full) doesn't work
 
 %% Decode TLE
 
-keplerElements = readTLE(file);
+keplerElements = readTLE(['files/',file,'.txt']);
 
 t = keplerElements(1,:)';   % [day]     time since first measurement
 a = keplerElements(2,:)';   % [m]       semi-major axis
