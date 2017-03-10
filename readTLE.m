9fab993be0bda000fc61375b3b7d8f3f4aff3036
mfacchinelli <michele.facchinelli@yahoo.it>
2017-03-10 14:21:47 +0100

    Turned into function and applied small changes

--- readTLE.m
+++ readTLE.m
@@ -1,13 +1,18 @@
-clear all; close all; clc;
+%  Purpose:
+%  Input:
+%   - 
+%  Output:
+%   - 
 
-%...Input file name
-% file = 'noaa-06.txt';       % NOAA 06   (full) doesn't work
-% file = 'noaa.txt';          % NOAA 06   (reduced) works
-file = 'zarya.txt';         % ISS       (reduced) works
+function kepler = readTLE(file)
+
+%...Constants
+mu = 398600.441e9;          % [m3/s2]   Earth gravitational parameter
+Te = 23*3600+56*60+4.1004;  % [s]       Earth sidereal day
 
 %...Read lines
 fileID = fopen(file,'r');
-read = fscanf(fileID,'%c'); % %s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n
+read = fscanf(fileID,'%c');
 fclose(fileID);
 
 %...Reshape
@@ -15,33 +20,40 @@ read = split(string(read));
 if mod(size(read,1),9) ~= 0, error('Something went wrong while reading the TLE file.'); end
 read = reshape(read,9,[]);
 
-%...Constants
-mu = 398600.441e9;          % [m3/s2]   Earth gravitational parameter
-Te = 23*3600+56*60+4.1004;  % [s]       Earth sidereal day
+%...Decode time (disregards leap years)
+time = char(read(4,1:2:end));
+year = str2double(string(time(:,1:2,:)));
+year(year>50) = 1900+year(year>50); % convert years to four digits
+year(year<50) = 2000+year(year<50); % ... (will only work until 2049)
+year = year - year(1); % convert year to years since first measurement
+days = str2double(string(time(:,3:end,:)));
+days = days - days(1); % convert days to days since first measurement
 
-%...Decode lines
-satID = read(3,1:2:end);                % [-]       satellite identifier
+%...Decode rest of lines
+% satID = read(3,1:2:end);                % [-]       satellite identifier
+t = year.*365+days;                     % [day]     time since first measurement
 i = str2double(read(3,2:2:end));        % [deg]     inclination
 O = str2double(read(4,2:2:end));        % [deg]     right ascension of ascending node
 e = str2double(read(5,2:2:end))./1e7;   % [-]       eccentricity
 o = str2double(read(6,2:2:end));        % [deg]     argument of perigee
-M = str2double(read(7,2:2:end));        % [deg]     mean anomaly
+MA = str2double(read(7,2:2:end));       % [deg]     mean anomaly
 n = str2double(read(8,2:2:end));        % [rad/s]   mean motion
 T = Te./n;                              % [s]       period
 a = ((T./(2*pi)).^2*mu).^(1/3);         % [m]       semi-major axis
-b = a.*sqrt(1-e.^2);                    % [m]       semi-minor axis
+% b = a.*sqrt(1-e.^2);                    % [m]       semi-minor axis
 
 %...Combine Keplerian elements
-kepler = vertcat(a./1000,e,i,O,o,M);
+kepler = vertcat(t,a,e,i,O,o,MA);
 
 %...Plot Keplerian elements
 figure;
-labels = {'a [km]','e [-]','i [deg]','\Omega [deg]','\omega [deg]','M [deg]'};
-for i = 1:size(kepler,1)
+labels = {'a [m]','e [-]','i [deg]','\Omega [deg]','\omega [deg]','M [deg]'};
+for i = 1:size(kepler,1)-1
     subplot(3,2,i)
-    plot(kepler(i,:))
-    xlabel('Time [?]')
+    plot(kepler(1,:),kepler(i+1,:))
+    xlabel('Time [day]')
     ylabel(labels{i})
+    xlim([kepler(1,1),kepler(1,end)])
     grid on
     set(gca,'FontSize',13)
 end
