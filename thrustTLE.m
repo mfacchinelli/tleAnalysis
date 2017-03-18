c059e1c17859c8aafb99100bb86bb79f1fd05f67
mfacchinelli <michele.facchinelli@yahoo.it>
2017-03-18 16:57:16 +0100

    Renamed and applied small improvements

--- thrustTLE.m
+++ thrustTLE.m
@@ -5,29 +5,33 @@
 %  Output:
 %   - N/A
 
-function thrustDetection(kepler,options)
+function thrustTLE(kepler,options)
+
+%...Extract options
+ignore = options.ignore;
 
 %...Change in orbital elements
-da = diff(kepler(:,2));
-de = diff(kepler(:,3));
-di = diff(kepler(:,4));
-dO = diff(kepler(:,5));
-do = diff(kepler(:,6));
+lower = floor(ignore*size(kepler,1));
+da = diff(kepler(lower:end,2));
+de = diff(kepler(lower:end,3));
+di = diff(kepler(lower:end,4));
+dO = diff(kepler(lower:end,5));
+do = diff(kepler(lower:end,6));
 
 %...Remove changes of 360 degrees from O and o
 % dO(diff(dO)>350) = dO(diff(dO)>350)+360;
 % dO(diff(dO)<-350) = dO(diff(dO)<-350)-360;
 
 %...Get statistics
-statTLE([da,de,di,dO,do],options);
+data = statTLE([da,de,di,dO,do],options);
 
 %...Detect thrust peaks
-[peaks_a_1,locs_a_1] = findpeaks(da,'MinPeakHeight',std(da)); % positive change
-[peaks_a_2,locs_a_2] = findpeaks(-da,'MinPeakHeight',std(da)); % negative change
-[peaks_e_1,locs_e_1] = findpeaks(de,'MinPeakHeight',std(de)); % positive change
-[peaks_e_2,locs_e_2] = findpeaks(-de,'MinPeakHeight',std(de)); % negative change
-[peaks_i_1,locs_i_1] = findpeaks(di,'MinPeakHeight',std(di)); % positive change
-[peaks_i_2,locs_i_2] = findpeaks(-di,'MinPeakHeight',std(di)); % negative change
+[peaks_a_1,locs_a_1] = findpeaks(da,'MinPeakHeight',data.a(1)); % positive change
+[peaks_a_2,locs_a_2] = findpeaks(-da,'MinPeakHeight',data.a(2)); % negative change
+[peaks_e_1,locs_e_1] = findpeaks(de,'MinPeakHeight',data.e(1)); % positive change
+[peaks_e_2,locs_e_2] = findpeaks(-de,'MinPeakHeight',data.e(2)); % negative change
+[peaks_i_1,locs_i_1] = findpeaks(di,'MinPeakHeight',data.i(1)); % positive change
+[peaks_i_2,locs_i_2] = findpeaks(-di,'MinPeakHeight',data.i(2)); % negative change
 
 %...Merge positive and negative values
 peaks_a = sort(vertcat(peaks_a_1,peaks_a_2));
