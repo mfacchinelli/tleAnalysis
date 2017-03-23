c1cf9883d794b9a2ff5885e5367ce17933e6de49
Michele Facchinelli <m.facchinelli@student.tudelft.nl>
2017-03-23 12:01:11 +0100

    Delete Cross.m

--- paul/Cross.m
+++
@@ -1,7 +0,0 @@
-function [v3] = Cross(v1,v2)
-%Cross Produces cross product of v1 and v2, and returns in v3
-    v3(1)=v1(2)*v2(3)-v1(3)*v2(2);
-	v3(2)=v1(3)*v2(1)-v1(1)*v2(3);
-	v3(3)=v1(1)*v2(2)-v1(2)*v2(1);
-    v3(4)=Magnitude(v3);
-end
