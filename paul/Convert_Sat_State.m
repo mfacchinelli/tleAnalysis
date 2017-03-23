d75b09acbab0c6a17fec54b674136984407be83f
Michele Facchinelli <m.facchinelli@student.tudelft.nl>
2017-03-23 12:01:01 +0100

    Delete Convert_Sat_State.m

--- paul/Convert_Sat_State.m
+++
@@ -1,8 +0,0 @@
-function [pos,vel] = Convert_Sat_State(pos,vel)
-%Convert_Sat_State Converts the satellite's position and velocity vectors 
-%from normalized values to km and km/sec
-    global XKMPER XMNPDA AE;
-    
-	pos = Scale_Vector(XKMPER/AE, pos);
-    vel = Scale_Vector(XKMPER/AE*XMNPDA/86400., vel);
-end
