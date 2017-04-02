254af5b0a7fb4acb587c5f9f592c54f2e51e5b1c
mfacchinelli <michele.facchinelli@yahoo.it>
2017-04-02 22:11:00 +0200

    Added GUI for input

--- functions/settings.m
+++ functions/settings.m
@@ -1,6 +1,6 @@
 %  MATLAB Function < settings >
 % 
-%  Purpose:	define settings globally
+%  Purpose:	define settings globally using a GUI
 %  Input:
 %   - N/A
 %  Output:
@@ -12,16 +12,26 @@ function options = settings()
 %   	false (0):  for sure satellite has no thrust
 %    	true  (1):  no available information/do not know
 
-filename = input('Please enter a valid NORAD identifier: ','s'); % ask for NORAD ID
+answer = inputdlg({'NORAD identifier:',...
+                   'Usage of thrust (T/F):',...
+                   'Show figures (T/F):',...
+                   'Initial data to ignore (%):',...
+                   'Safety factor for detection (-):',...
+                   'Separation between thrust periods (day):',...
+                   'Number of steps for propagation (-):',...
+                   "Apply Chauvenet's criterion (T/F):"},...
+                   'TLE Analysis',...
+                   1,...
+                   {'32789','true','true','5','1.05','50','1','true'},'on');
 
-options = struct('file',    ['files/',filename,'.txt'],... % adapt name
-                 'thrust',  true,... % (see above)
-                 'showfig', true,... % show figures
-                 'ignore',  0.05,... % ignore first XX percent of data
-                 'factor',  1.05,... % safety factor for thrust detection
-                 'limit',   50,... % limit for days of separations between maneuvers
-                 'offset',  7,... % number of steps to take between observations
-                 'outlier', true); % apply Chauvenet's criterion
+options = struct('file',    ['files/',answer{1},'.txt'],... % adapt name
+                 'thrust',  strcmpi(answer{2},'true'),...   % (see above)
+                 'showfig', strcmpi(answer{3},'true'),...   % show figures
+                 'ignore',  str2double(answer{4})/100,...	% ignore first XX percent of data
+                 'factor',  str2double(answer{5}),...       % safety factor for thrust detection
+                 'limit',   str2double(answer{6}),...       % limit for days of separations between maneuvers
+                 'offset',  str2double(answer{7}),...       % number of steps to take between observations
+                 'outlier', strcmpi(answer{8},'true'));     % apply Chauvenet's criterion
 
 %...Make sure selection is intentional
 if options.thrust == false
@@ -39,13 +49,13 @@ load('files/satData.mat');
 
 %...Check if satellite is in file
 try 
-    satellites(filename);
+    satellites(answer{1});
 catch
     %...Ask for data
     mass = input('Insert satellite mass (kg): ');
     units = input('Insert satellite number of units (U): ');
     panels = input('Insert solar panel logical (T/F): ');
-    satellites(filename) = [mass,units,panels];
+    satellites(answer{1}) = [mass,units,panels];
     
     %...Save with new data
     save('files/satData.mat','satellites');
